70948b9e230240ba834782b5e6826223fc6b0583
Denis Ilyushin <to@denisilyushin.ru>
2023-06-06 00:19:31 +0300

    feat: Настроить работу попапов
    Добавьте императивные обработчики
    Чтобы добавить первую интерактивность, а заодно проверить, правильно ли вы портировали разметку попапов, внутри компонента Main добавьте следующие обработчики:
    handleEditAvatarClick
    handleEditProfileClick
    handleAddPlaceClick
    Здесь мы немного схитрим: внутри этих обработчиков временно используйте привычный императивный подход (с querySelector и classList.add), чтобы задавать CSS-класс popup_is-opened элементу нужного попапа.
    Теперь присвойте эти обработчики нужным элементам в JSX и убедитесь, что все три попапа открываются

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,28 @@
 export default function Main() {
+
+  function handleEditProfileClick() {
+    document.querySelector('.popup_type_edit-profile').classList.add('popup_opened')
+  }
+  function handleEditAvatarClick() {
+    document.querySelector('.popup_type_update-avatar').classList.add('popup_opened')
+  }
+  function handleAddMestoClick() {
+    document.querySelector('.popup_type_add-mesto').classList.add('popup_opened')
+  }
+
   return (
     <main className="content">
       <section className="profile" id="profile">
         <div className="profile__avatar-container">
           <img className="profile__avatar" src="#" alt="Аватар пользователя" />
-          <button className="profile__avatar-edit-button" type="button"></button>
+          <button onClick={handleEditAvatarClick} className="profile__avatar-edit-button" type="button"></button>
         </div>
         <div className="profile__info">
           <h1 className="profile__user-name"></h1>
           <p className="profile__user-job"></p>
-          <button className=" profile__edit-button" type="button"></button>
+          <button onClick={handleEditProfileClick} className=" profile__edit-button" type="button"></button>
         </div>
-        <button className="profile__add-button" type="button"></button>
+        <button onClick={handleAddMestoClick} className="profile__add-button" type="button"></button>
       </section>
       <section className="places" id="places">
         <ul className="places__list">
